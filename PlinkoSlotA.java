d53ed58395cce04b8fabec330c7b19d375e24aa6
AoW278 <aow252@gmail.com>
2018-12-05 23:45:05 -0500

    Update PlinkoSlotA.java

--- PlinkoSlotA.java
+++ PlinkoSlotA.java
@@ -1,6 +1,7 @@
 import java.util.Random;
 
 public class PLINKOA {
+	// The containers (only containing 7 because Slot A cannot reach the 100 and 500 containers on the right)
 	static int count100 = 0;
 	static int count500 = 0;
 	static int count1000 = 0;
@@ -13,7 +14,13 @@ public class PLINKOA {
 
 		Random rand = new Random();
 
+		// A million Plinko chips dropped from Slot A.
+		// left = 1, right = 0
+		
 		for (int i = 1; i <= 1000000; i++) {
+			
+		// The first peg will have to go right, as there is a wall to the left
+			
 			int peg1 = 0;
 			int peg2 = rand.nextInt(2);
 			int peg3 = rand.nextInt(2);
@@ -26,7 +33,12 @@ public class PLINKOA {
 			int peg10 = rand.nextInt(2);
 			int peg11 = rand.nextInt(2);
 			int peg12 = rand.nextInt(2);
-
+			
+			/**
+			Below took regard of the walls. When adding the first two pegs, if the sum is 1, the chip
+			hits a wall. If so, the third peg will have to go right. 
+			**/
+			
 			if (peg1 + peg2 == 1) {
 				peg3 = 0;
 			}
@@ -42,7 +54,9 @@ public class PLINKOA {
 			if (peg1 + peg2 + peg3 + peg4 + peg5 + peg6 + peg7 + peg8 + peg9 + peg10 == 5) {
 				peg11 = 0;
 			}
-
+			
+			// pattern of reaching to each of the containers 
+			
 			int totalLeft = peg1 + peg2 + peg3 + peg4 + peg5 + peg6 + peg7 + peg8 + peg9 + peg10 + peg11 + peg12;
 
 			if (totalLeft == 6) {
@@ -63,7 +77,9 @@ public class PLINKOA {
 			}
 
 		}
-
+		
+		// count of the chips for each container
+		
 		System.out.println("Amount of 100's: " + count100);
 		System.out.println("Amount of 500's: " + count500);
 		System.out.println("Amount of 1000's: " + count1000);
@@ -74,6 +90,8 @@ public class PLINKOA {
 		System.out.println("Amount of 500's: " + 0);
 		System.out.println("Amount of 100's: " + 0);
 		System.out.println();
+		
+		// probability of the chips for each container
 
 		System.out.println("Percentage of 100's: " + (double) count100 / 1000000);
 		System.out.println("Percentage of 500's: " + (double) count500 / 1000000);
@@ -86,6 +104,7 @@ public class PLINKOA {
 		System.out.println("Percentage of 100's: " + 0);
 
 	}
+	// method that allows the main class to compare the probability of getting 10,000 from the other 8 classes
 	public static double percentage10000A() {
 		return (double) count10000 / 1000000;
 	}
